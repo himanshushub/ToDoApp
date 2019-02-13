bd55d1cb09f4b84c36ee328a64a57c3115419d1e
himanshushub <himanshushub8@gmail.com>
2019-02-13 08:12:43 +0530

    2nd Finished Oauth with mongo

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: proccess.env.GOOGLE_SECRET,
-    mongoURI: proccess.env.MONGOURI,
-    cookieKey: proccess.env.COOKIEKEY
+    googleClientSecret: process.env.GOOGLE_SECRET,
+    mongoURI: process.env.MONGOURI,
+    cookieKey: process.env.COOKIEKEY
 }
