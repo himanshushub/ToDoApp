555822a91eb2aed2f3a9113a9a9e23e0a90ef1ac
himanshushub <himanshushub8@gmail.com>
2019-02-19 21:58:20 +0530

    Added proxy and script for heroku to run the React side of app properly

--- index.js
+++ index.js
@@ -30,7 +30,19 @@ addTodo(app);
 authRoutes(app);
 
 
+if(process.env.NODE_ENV === 'production'){
+    //Express will serve up the productionn assets
+    //like our main.js file or main.css file
+    app.use(express.static('client/build'));
 
+
+    //Express will serve up the index.html file
+    //if it does not recognize the route
+    const path = require('path');
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 app.get('/', (req, res) => {
